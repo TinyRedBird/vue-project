80817f90833ebd015f2eab0cad9651209f2600df
luliye <aa15877153704@outlook.com>
2024-10-06 22:36:21 +0800

    添加依赖unplugin-auto-import

--- vite.config.js
+++ vite.config.js
@@ -11,13 +11,12 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 export default defineConfig({
   plugins: [
     vue(),
-      AutoImport({
-        resolvers: [ElementPlusResolver()],
-      }),
-      Components({
-        resolvers: [ElementPlusResolver()],
-      }),
-    
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()]
+    })
   ],
   resolve: {
     alias: {
