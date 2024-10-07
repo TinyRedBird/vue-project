b935d1063fb95235d6cd26c226d350d4a43bcb70
luliye <aa15877153704@outlook.com>
2024-10-07 20:33:12 +0800

    .

--- src/apis/sale.js
+++ src/apis/sale.js
@@ -2,10 +2,13 @@ import request from '@/utils/request'
 
 export const getSaleGoodsList = (params) => {
   return request.get('/sale', { params: params })
-  // console.log(params, 22)
+  // console.log(params)
 }
 
 export const getSaleDetail = (params) => {
   return request.get('/sale/detail', { params: params })
 }
 
+export const getSaleCompare = (params) => {
+  return request.get('/sale/compare', { params: params })
+}
